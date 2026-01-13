fab461437be1365181b06e3d83241b52ac74c42c
Yusuf Kağan Kozan <9kaanv4@gmail.com>
2026-01-13 15:17:03 +0300

    Add files via upload

---
+++ app_data.dart/app_data.dart
@@ -0,0 +1,82 @@
+// lib/data/app_data.dart
+
+// Haberler Listesi
+final List<Map<String, String>> newsList = [
+  {
+    "title": "2024-2025 Akademik Yılı Açılış Töreni",
+    "image":
+        "https://images.unsplash.com/photo-1523050854058-8df90110c9f1?q=80&w=1000&auto=format&fit=crop",
+  },
+  {
+    "title": "Yemekhane Burs Başvuruları Başladı",
+    "image":
+        "https://images.unsplash.com/photo-1555244162-803834f70033?q=80&w=1000&auto=format&fit=crop",
+  },
+  {
+    "title": "Teknofest Başvuruları İçin Son Gün",
+    "image":
+        "https://images.unsplash.com/photo-1531482615713-2afd69097998?q=80&w=1000&auto=format&fit=crop",
+  },
+];
+
+// Bölüm Listesi ve Linkleri
+final List<Map<String, String>> departments = [
+  // --- MÜHENDİSLİK ---
+  {
+    'name': 'Bilgisayar Mühendisliği',
+    'url':
+        'https://bilmuh.ozal.edu.tr/contentFiles/1758460652604.Bilmuh_dersprog_2025-2026_G%C3%9CZ_TASLAK_SON.pdf',
+  },
+  {'name': 'Biyomühendislik', 'url': 'https://ozal.edu.tr/ornek_program.pdf'},
+  {
+    'name': 'Elektrik-Elektronik Mühendisliği',
+    'url': 'https://ozal.edu.tr/ornek_program.pdf',
+  },
+  {
+    'name': 'İnşaat Mühendisliği',
+    'url': 'https://ozal.edu.tr/ornek_program.pdf',
+  },
+  {
+    'name': 'Yazılım Mühendisliği',
+    'url': 'https://ozal.edu.tr/ornek_program.pdf',
+  },
+  {
+    'name': 'Biyosistem Mühendisliği',
+    'url': 'https://ozal.edu.tr/ornek_program.pdf',
+  },
+  {
+    'name': 'Su Ürünleri Mühendisliği',
+    'url': 'https://ozal.edu.tr/ornek_program.pdf',
+  },
+
+  // --- SAĞLIK ---
+  {
+    'name': 'Acil Yardım ve Afet Yönetimi',
+    'url': 'https://ozal.edu.tr/ornek_program.pdf',
+  },
+  {
+    'name': 'Beslenme ve Diyetetik',
+    'url': 'https://ozal.edu.tr/ornek_program.pdf',
+  },
+  {'name': 'Ebelik', 'url': 'https://ozal.edu.tr/ornek_program.pdf'},
+  {
+    'name': 'Egzersiz ve Spor Bilimleri',
+    'url': 'https://ozal.edu.tr/ornek_program.pdf',
+  },
+  {'name': 'Gerontoloji', 'url': 'https://ozal.edu.tr/ornek_program.pdf'},
+  {'name': 'Hemşirelik', 'url': 'https://ozal.edu.tr/ornek_program.pdf'},
+  {'name': 'Sosyal Hizmet', 'url': 'https://ozal.edu.tr/ornek_program.pdf'},
+  {'name': 'Tıp Fakültesi', 'url': 'https://ozal.edu.tr/ornek_program.pdf'},
+
+  // --- DİĞERLERİ ---
+  {
+    'name': 'Görsel İletişim Tasarım',
+    'url': 'https://ozal.edu.tr/ornek_program.pdf',
+  },
+  {'name': 'İç Mimarlık', 'url': 'https://ozal.edu.tr/ornek_program.pdf'},
+  {
+    'name': 'Yönetim Bilişim Sistemleri',
+    'url': 'https://ozal.edu.tr/ornek_program.pdf',
+  },
+  // ... Listeyi istediğin kadar uzatabilirsin, ana kodun kirlenmez.
+];
